f03decbe534ac47eaa07b55eecc51088f45ace96
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-06 22:33:14 +0300

    add code for start

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import os
+
+from src.views import (get_operations_from_xls, get_list_operations_from_dataframe, get_operations_by_date,
+                       get_result_list_dictionaries)
+from src.services import get_operations_by_find_str_put_json
+from src.reports import spending_by_category
+
+FILE_OPERATIONS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'operations.xls')
+
+
+def main():
+    # получаем данные из файла эксель в формате датафрейм
+    df = get_operations_from_xls(FILE_OPERATIONS)
+
+    # преобразовываем датафрейм в список словарей
+    operations = get_list_operations_from_dataframe(df)
+
+    # получаем операции за определённый период
+    transactions = get_operations_by_date(operations, "2021-12-02 21:45:00")
+
+    # получаем json-ответ с запрашиваемыми данными
+    print(get_result_list_dictionaries(transactions))
+
+    # получаем данные по строке поиска в описании или категории и записываем их в файл
+    get_operations_by_find_str_put_json(operations, "Госуслуги")
+
+    # получаем траты по заданной категории за последние 3 месяца и отправляем отчет в файл report.xlsx
+    print(spending_by_category(df, "Госуслуги", "24.12.2021"))
+
+
+if __name__ == '__main__':
+    main()
