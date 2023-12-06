21a85ed0b69b348badd4d05c9a2ec1358aa4364f
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-06 22:32:27 +0300

    add code

--- src/services.py
+++ src/services.py
@@ -3,23 +3,20 @@ import logging
 import pytest
 from views import get_operations_from_xls
 
+logger = logging.getLogger(__name__)
 
 BY_SEARCH_FILE = 'list_search.json'
 
 
-def get_operations_by_find_str_put_json(search_str: str) -> None:
+def get_operations_by_find_str_put_json(data: list[dict], search_str: str) -> None:
     """
     Функция принимает на вход строку поиска, и выводит список транзакций, содержащими запрос в описании или категории.
+    :param data: список словарей
     :param search_str: строка поиска
-    :return: список транзакций, удовлетворяющий условиям
     """
-    data = get_operations_from_xls("../data/operations.xls")
-
     results = [item for item in data if search_str.strip() in (item["Описание"], item["Категория"])]
 
     with open(BY_SEARCH_FILE, 'w', encoding='utf-8') as f:
         json.dump(results, f, ensure_ascii=False, indent=4)
 
-
-if __name__ == "__main__":
-    print(get_operations_by_find_str_put_json("Госуслуги"))
+        logger.info("Данные записаны в файл")
